bafb9110ee82891aded10e083f0ec8e40c4306de
W.Janidu Pesara <janidu.pesara.work@gmail.com>
2026-05-09 17:42:25 +0530

    add prefectid.php

---
+++ prefectid.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>taxila prifect guild</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="navbar">
+            <div class="box box-left " ><img src="Taxila-logo.png" alt="taxila_logo" class="logos"></div>
+            <div class="box box-middle">TAXILA CENTREL COLLEGE PREFECT GUILD</div>
+            <div class="box box-right "><img src="Prefects Logo .png" alt="prefect_logo" class="logos"></div>
+
+        </div>
+
+        <div class="container">
+            <div class="box-left2"></div>
+            <div class="box-middle2">
+                <div class="container3">
+                    <div class="centerbox">
+                        <div>
+                        <label for="prefectid">Enter the prefect id</label><br>
+                        <input type="text" name="prefectid" id="prefectid">
+
+                        <br><br>
+                        <a href="duty.php">
+                            <button>confirm</button>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="box-right2"></div>
+
+        </div>
+
+    </body>
+
+</html>
