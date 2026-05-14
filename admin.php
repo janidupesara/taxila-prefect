f9c05256302e037b0fe48b9958ac627b80ee0d30
W.Janidu Pesara <janidu.pesara.work@gmail.com>
2026-05-14 16:14:45 +0530

    add admin.php

---
+++ admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>taxila prifect guild</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="navbar">
+            <div class="box box-left " ><img src="Taxila-logo.png" alt="taxila_logo" class="logos"></div>
+            <div class="box box-middle">TAXILA CENTREL COLLEGE PREFECT GUILD</div>
+            <div class="box box-right "><img src="Prefects Logo .png" alt="prefect_logo" class="logos"></div>
+
+        </div>
+
+        <div class="container">
+            <div class="box-left2"></div>
+            <div class="box-middle2">
+                <div class="container2">
+                    <a href="enter_prefect.php">
+                        <button class="button2">enter_prefects </button>
+                    </a>
+                    <a href="enter_duty.php">
+                        <button  class="button2">enter_duty </button>
+                    </a>
+                    <a href="mark_duty.php">
+                        <button  class="button2">mark_duty </button>
+                    </a>
+                    <a href="eventhanddle.php">
+                        <button  class="button2">event_handdle</button>
+                    </a>
+
+                </div>
+               
+            </div>
+            <div class="box-right2"></div>
+
+        </div>
+            
+    </body>
+
+</html>
