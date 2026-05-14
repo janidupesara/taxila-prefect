c31f9f07d53d69f5c97a8e2bce4e63945f10ea31
W.Janidu Pesara <janidu.pesara.work@gmail.com>
2026-05-14 16:14:08 +0530

    add duty.php

---
+++ duty.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>taxila prifect guild</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="navbar">
+            <div class="box box-left " ><img src="Taxila-logo.png" alt="taxila_logo" class="logos"></div>
+            <div class="box box-middle">TAXILA CENTREL COLLEGE PREFECT GUILD</div>
+            <div class="box box-right "><img src="Prefects Logo .png" alt="prefect_logo" class="logos"></div>
+
+        </div>
+
+        <div class="container">
+            <div class="box-left2"></div>
+            <div class="box-middle2">
+                <div class="container2">
+                    <a href="dalyduty.php">
+                        <button class="button2">daly_duty </button>
+                    </a>
+                    <a href="spetialduty.php">
+                        <button  class="button2">spetial_duty </button>
+                    </a>
+                    <a href="events.php">
+                        <button  class="button2">events </button>
+                    </a>
+                    <a href="portfolio.php">
+                        <button  class="button2">portfolio </button>
+                    </a>
+
+                    
+                </div>
+               
+            </div>
+            <div class="box-right2"></div>
+
+        </div>
+            
+    </body>
+
+</html>
