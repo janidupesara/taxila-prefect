479f47eb1382887208b47ca683b1316a9424082a
W.Janidu Pesara <janidu.pesara.work@gmail.com>
2026-05-20 10:49:38 +0530

    add enter_prefect.php

---
+++ enter_prefect.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>taxila prifect guild</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="navbar">
+            <div class="box box-left " ><img src="Taxila-logo.png" alt="taxila_logo" class="logos"></div>
+            <div class="box box-middle">TAXILA CENTREL COLLEGE PREFECT GUILD</div>
+            <div class="box box-right "><img src="Prefects Logo .png" alt="prefect_logo" class="logos"></div>
+
+        </div>
+
+        <div class="container">
+            <div class="box-left2"></div>
+            <div class="box-middle2">
+                <div class="container3">
+                    <div class="centerbox">
+                        <div>
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" placeholder="enter name">
+                            <br>
+
+                            <label for="catogory">catogory</label><br>
+                            <label>
+                                <input type="radio" name="catogory" id="catogory"> senior 
+                            </label>
+                            <br>
+                            <label>
+                                <input type="radio" name="catogory" id="catogory"> junior 
+                            </label>
+                            
+                            <br>
+
+                            <label for="class">Class</label>
+                            <input type="text" name="class" id="class" placeholder="enter class">
+                            <br>
+
+                            <label for="prefectid">prefectid</label>
+                            <input type="text" name="prefectid" id="prefectid">
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <div class="box-right2"></div>
+
+    </div>
+
+     
+        
+        
+    </body>
+
+</html>
